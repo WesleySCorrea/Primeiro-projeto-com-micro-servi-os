3f47a8e81246f72e653fb4d50cf17bf562c88c55
unknown <Wesleyscorrea@hotmail.com>
2022-09-28 20:04:42 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/wesleyscorrea/hruser/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.wesleyscorrea.hruser.repositories;
+
+import com.wesleyscorrea.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
